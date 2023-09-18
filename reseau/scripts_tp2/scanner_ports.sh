eb6dbc2bbac764b8b11dffe04b716057bcea5b42
Siwa12100 <jean.marcillac12@gmail.com>
2023-09-18 10:21:54 +0200

    travail sur le tp 2 de réseau

--- reseau/scripts_tp2/scanner_ports.sh
+++ reseau/scripts_tp2/scanner_ports.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-host="127.0.0.1" # Remplacez par l'adresse IP de la cible que vous souhaitez scanner
+# Vérifie s'il y a un argument passé en ligne de commande
+if [ $# -eq 1 ]; then
+  host="$1" # Utilise l'adresse IP passée en argument
+else
+  host="localhost" # Utilise localhost par défaut si aucun argument n'est passé
+fi
+
+echo "Ports ouverts de la machine ayant l'IP $host"
+
 ports=$(seq 0 1024) # Plage de ports à scanner (0 à 1024 dans ce cas)
 
 for port in $ports
 do
   (socat -v -t 1 TCP:$host:$port OPEN:/dev/null 2>/dev/null) && echo "Port $port ouvert"
 done
+
