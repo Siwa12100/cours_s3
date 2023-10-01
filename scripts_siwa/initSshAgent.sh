3676f6eff3a403220ea8196901c3128ac11795f5
Siwa12100 <jean.marcillac12@gmail.com>
2023-10-01 16:40:47 +0200

    travail sur les scripts

--- scripts_siwa/initSshAgent.sh
+++ scripts_siwa/initSshAgent.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# Activer l'agent SSH
-eval $(ssh-agent -s)
+# Vérifier si l'agent SSH est activé
+if [ -n "$SSH_AUTH_SOCK" ]; then
+    echo "[Script Siwa initSshAgent.sh] : Agent SSH bien activé préalablement."
+else
+    echo "[Script Siwa initSshAgent.sh] : L'agent SSH n'est pas activé, pensez à le faire dans le fichier de configuration du shell."
+    exit 1
+fi
 
 # Fonction pour ajouter une clé privée à l'agent SSH
 function add_key_to_agent {
@@ -9,42 +14,23 @@ function add_key_to_agent {
     echo "Clé privée ajoutée à l'agent SSH : $1"
 }
 
-# Si des chemins vers des clés privées sont passés en paramètres
-if [ $# -gt 0 ]; then
+# Si l'option -g est passée en paramètre
+if [ "$1" == "-g" ]; then
+    echo "[Script Siwa initSshAgent.sh] : Exécution du script en mode github."
+    shift # Supprimer l'option -g des paramètres
+    while IFS= read -r -d $'\0' key_file; do
+        add_key_to_agent "$key_file"
+    done < <(find "$HOME/.ssh" -maxdepth 1 -type f -name '*github*' -not -name '*.pub' -print0)
+elif [ $# -eq 0 ]; then
+    echo "[Script Siwa initSshAgent.sh] : Exécution du script sans aucun paramètre."
+else
+    # Si des chemins vers des clés privées sont passés en paramètres
     for key_file in "$@"; do
         if [[ "$key_file" != *.pub ]]; then
             add_key_to_agent "$key_file"
         fi
     done
-else
-    # Si aucun chemin n'est passé en paramètre, parcourir le dossier ~/.ssh/
-    ssh_dir="$HOME/.ssh"
-    if [ -d "$ssh_dir" ]; then
-        for key_file in "$ssh_dir"/*; do
-            if [[ "$key_file" != *.pub ]]; then
-                add_key_to_agent "$key_file"
-            fi
-        done
-    else
-        echo "Le dossier ~/.ssh/ n'existe pas."
-        exit 1
-    fi
 fi
 
 
 
-
-# ===================================
-# eval $(ssh-agent -s)
-
-# if ! ssh-add "$1" 
-#     then
-#         for cle in "~/.ssh/*"
-#             do 
-#                 echo "$cle"
-#                 if ! grep '.pub' 
-#                     then 
-#                         echo " bonne clé : $cle " 
-#                 fi
-#             done
-#     fi
