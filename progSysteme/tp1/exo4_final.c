952a19827a1c47e87130cc87ab01a6ae56ae29d4
Siwa12100 <jean.marcillac12@gmail.com>
2023-10-15 21:19:39 +0200

    fin du tp 1 de prog Système

---
+++ progSysteme/tp1/exo4_final.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <time.h>
+#include <sys/wait.h>
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Utilisation : %s N\n", argv[0]);
+        return 1;
+    }
+
+    int N = atoi(argv[1]);
+    if (N <= 0) {
+        fprintf(stderr, "N doit être un entier positif.\n");
+        return 1;
+    }
+
+    pid_t pid;
+    int i, etat;
+    struct timespec t;
+
+    for (i = 0; i < N; i++) {
+        switch (pid = fork()) {
+            case -1: /* Oups !!! fork n'a pas marché ! */
+                perror("fork");
+                exit(errno);
+
+            case 0: /* Code du fils */
+                printf("[fils %d]: je suis le fils de pid %d\n", i, getpid());
+                for (int j = 0; j < 10; j++) {
+                    sleep(1); // Délai de 1 seconde entre les printf
+                    printf("[fils %d]: %d\n", i, j);
+                }
+                exit(2);
+                break;
+
+            default: /* Code du père */
+                break;
+        }
+    }
+
+    for (i = 0; i < N; i++) {
+        if ((pid = wait(&etat)) == -1) {
+            perror("pb avec le wait");
+            exit(errno);
+        }
+        if (WIFEXITED(etat))
+            printf("[pere]: mon fils %d a retourne le code %d\n", pid, WEXITSTATUS(etat));
+        else
+            printf("[pere]: mon fils %d s est mal termine\n", pid);
+    }
+
+    printf("[pere]: Fin du processus pere de pid %d.\n", getpid());
+
+    return 0;
+}
