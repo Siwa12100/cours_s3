4669b7bff4b2b56d61031e44bfce51bde957160c
Siwa12100 <jean.marcillac12@gmail.com>
2023-11-12 17:20:31 +0100

    map laptop

---
+++ temporaire/mvc/mvc_PSR4_twig/modeles/Simplemodel.php
@@ -0,0 +1,15 @@
+<?php
+namespace modeles;
+
+class Simplemodel
+{
+    //constructeur
+    //vide
+
+    public function get_data() :string
+    {
+        return 'Mon modèle ne fait rien';
+    }
+
+    //fin modèles
+}
