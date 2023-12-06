641f45b7f9340ed6aab905385273d1eef38dc454
Siwa12100 <jean.marcillac12@gmail.com>
2023-12-06 14:15:26 +0100

    :dizzy: Maj côté laptop

--- projetBlazorSiwa/projetBlazorSiwa/Pages/List.razor.cs
+++ projetBlazorSiwa/projetBlazorSiwa/Pages/List.razor.cs
@@ -1,7 +1,22 @@
-﻿namespace projetBlazorSiwa.Pages
+﻿using Microsoft.AspNetCore.Components;
+using projetBlazorSiwa.Models;
+
+namespace projetBlazorSiwa.Pages
 {
     public partial class List
     {
+        private Item[] items;
+
+        [Inject]
+        public HttpClient Http { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            items = await Http.GetFromJsonAsync<Item[]>($"{NavigationManager.BaseUri}fake-data.json");
+        }
 
     }
 }
--- projetBlazorSiwa/projetBlazorSiwa/obj/Debug/net7.0/projetBlazorSiwa.AssemblyInfo.cs
+++ projetBlazorSiwa/projetBlazorSiwa/obj/Debug/net7.0/projetBlazorSiwa.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projetBlazorSiwa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe91726bdd87a462e479fdb7453d1c208962d8bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("projetBlazorSiwa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projetBlazorSiwa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
